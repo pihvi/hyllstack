b61891cb902051c9b43689e499f96e872b6d01a8
Antti Mattila <antti.mattila@reaktor.fi>
2018-07-15 13:30:45 +0300

    kurssia

--- osa1/src/index.js
+++ osa1/src/index.js
@@ -1,8 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const App = () => {
+  console.log('Hello from komponentti')
+  return (
+    <div>
+      <p>Hello world</p>
+    </div>
+  )
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'))
