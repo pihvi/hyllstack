deac394d14db37f41857ba044ce2b682ea77fbb8
Antti Mattila <antti.mattila@reaktor.fi>
2018-07-15 14:31:42 +0300

    osat

--- osa1/src/teht-1.4.js
+++ osa1/src/teht-1.4.js
@@ -12,28 +12,26 @@ const Sisalto = p =>
 const Yhteensa = p => <p>yhteensä {p.osat[0].tehtavia + p.osat[1].tehtavia + p.osat[2].tehtavia} tehtävää</p>
 
 const App = () => {
-  const kurssi = {
-    nimi: 'Half Stack -sovelluskehitys',
-    osat: [
-      {
-        nimi: 'Reactin perusteet',
-        tehtavia: 10
-      },
-      {
-        nimi: 'Tiedonvälitys propseilla',
-        tehtavia: 7
-      },
-      {
-        nimi: 'Komponenttien tila',
-        tehtavia: 14
-      }
-    ]
-  }
+  const kurssi = 'Half Stack -sovelluskehitys'
+  const osat = [
+    {
+      nimi: 'Reactin perusteet',
+      tehtavia: 10
+    },
+    {
+      nimi: 'Tiedonvälitys propseilla',
+      tehtavia: 7
+    },
+    {
+      nimi: 'Komponenttien tila',
+      tehtavia: 14
+    }
+  ]
   return (
     <div>
-      <Otsikko kurssi={kurssi.nimi}/>
-      <Sisalto osat={kurssi.osat}/>
-      <Yhteensa osat={kurssi.osat}/>
+      <Otsikko kurssi={kurssi}/>
+      <Sisalto osat={osat}/>
+      <Yhteensa osat={osat}/>
     </div>
   )
 }
