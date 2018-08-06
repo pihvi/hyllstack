c4e45dc8b98135124c4ea004760e8a7e8e6fd74e
Antti Mattila <antti.mattila@reaktor.fi>
2018-08-07 00:28:29 +0300

    deploy front and use proxy locally

--- osa2/src/services/phonebook.js
+++ osa2/src/services/phonebook.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const base = 'http://localhost:3001/api'
+const base = '/api'
 
 const add = entry => axios.post(base + '/persons', entry)
 const del = id => axios.delete(base + '/persons/' + id)
