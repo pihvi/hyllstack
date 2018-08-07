5cd8a73091d3a5c79ecd7ff4caa7b916b394c530
Antti Mattila <antti.mattila@reaktor.fi>
2018-08-07 11:55:01 +0300

    ESlint

---
+++ osa3/teht-3.20.js
@@ -0,0 +1,87 @@
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const Person = require('./models/person.js')
+
+app.use(cors())
+app.use(bodyParser.json())
+morgan.token('body', req => JSON.stringify(req.body))
+app.use(morgan(':method :url :body :status :res[content-length] - :response-time ms'))
+
+let persons = [{
+  "name": "Arto Hellas",
+  "num": "040-123456",
+  "id": 1
+}, {
+  "name": "Martti Tienari",
+  "num": "040-123456",
+  "id": 2
+}, {
+  "name": "Arto Järvinen",
+  "num": "040-123456",
+  "id": 3
+}, {
+  "name": "Lea Kutvonen",
+  "num": "040-123456",
+  "id": 4
+}]
+
+
+app.get('/api/persons', (req, res) => {
+  Person
+    .find({})
+    .then(result => {
+      res.send(result.map(x => ({
+        id: x._id,
+        name: x.name,
+        num: x.num
+      })))
+    })
+})
+
+app.get('/api/persons/:id', (req, res) => {
+  const found = persons.find(p => p.id === Number(req.params.id))
+  res.status(found ? 200 : 404).send(found)
+})
+
+// curl -XDELETE http://localhost:3001/api/persons/3
+app.delete('/api/persons/:id', (req, res) => {
+  Person.remove({_id: req.params.id}, (err) => res.status(204).end())
+})
+
+// curl -XPOST http://localhost:3001/api/persons -d '{"name":"john","num":"123455"}' -H 'Content-Type: application/json'
+app.post('/api/persons', (req, res) => {
+  const pers = {
+    name: req.body.name,
+    num: req.body.num
+  }
+  if (!pers.name || !pers.num) {
+    res.status(400).send({message: 'missing name or number'})
+  } else {
+    new Person(pers)
+      .save()
+      .then(response => {
+        res.staus(204).end()
+      })
+  }
+})
+
+app.get('/info', (req, res) => {
+  res.send(`
+    <html>
+      <body>
+        <p>puhelinluettelossa ${persons.length} henkilön tiedot</p>
+        <p>${new Date}</p>
+      </body>
+    </html>
+  `)
+})
+
+app.use(express.static('build'))
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
