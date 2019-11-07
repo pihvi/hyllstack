b01ee700a64fc8fb8494335aecb0182aa1fcff8c
Antti Mattila <72422+pihvi@users.noreply.github.com>
2019-11-07 12:16:46 +0200

    8.2 all books

--- osa8/library-backend.js
+++ osa8/library-backend.js
@@ -85,10 +85,19 @@ let books = [
 ]
 
 const typeDefs = gql`
+  type Book {
+    title: String!
+    published: Int!
+    author: String!
+    genres: [String!]!
+    id: ID!
+  }
+
   type Query {
     hello: String!
     bookCount: Int!
     authorCount: Int!
+    allBooks: [Book!]!
   }
 `
 
@@ -96,7 +105,8 @@ const resolvers = {
   Query: {
     hello: () => 'world',
     bookCount: () => books.length,
-    authorCount: () => authors.length
+    authorCount: () => authors.length,
+    allBooks: () => books,
   }
 }
 
