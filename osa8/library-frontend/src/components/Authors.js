9dc49f4f0d1fd1c0dae9ff7fea1e1707a55e080c
Antti Mattila <72422+pihvi@users.noreply.github.com>
2019-12-07 16:56:16 +0200

    reading from server works

--- osa8/library-frontend/src/components/Authors.js
+++ osa8/library-frontend/src/components/Authors.js
@@ -1,32 +1,45 @@
-import React, { useState } from 'react'
+import React from 'react'
+import {useQuery} from '@apollo/react-hooks'
+import {gql} from 'apollo-boost'
+
+const query = gql`
+{
+  allAuthors  {
+    name
+  }
+}
+`
 
 const Authors = (props) => {
-  if (!props.show) {
-    return null
-  }
-  const authors = []
+  const {loading, error, data} = useQuery(query)
+
+  if (!props.show) return null
+  if (loading) return 'Loading...'
+  if (error) return `Error! ${error.message}`
+
+  const authors = data.allAuthors
 
   return (
     <div>
       <h2>authors</h2>
       <table>
         <tbody>
-          <tr>
-            <th></th>
-            <th>
-              born
-            </th>
-            <th>
-              books
-            </th>
+        <tr>
+          <th></th>
+          <th>
+            born
+          </th>
+          <th>
+            books
+          </th>
+        </tr>
+        {authors.map(a =>
+          <tr key={a.name}>
+            <td>{a.name}</td>
+            <td>{a.born}</td>
+            <td>{a.bookCount}</td>
           </tr>
-          {authors.map(a =>
-            <tr key={a.name}>
-              <td>{a.name}</td>
-              <td>{a.born}</td>
-              <td>{a.bookCount}</td>
-            </tr>
-          )}
+        )}
         </tbody>
       </table>
 
