7d9c17df33c1d1d2894d5c1ef64623a999365568
Antti Mattila <72422+pihvi@users.noreply.github.com>
2019-12-08 23:07:50 +0200

    Use mongo

--- osa8/teht_8.13/library-backend.js
+++ osa8/teht_8.13/library-backend.js
@@ -1,5 +1,20 @@
 const { ApolloServer, gql } = require('apollo-server')
 const uuidv1 = require('uuid/v1')
+const mongoose = require('mongoose')
+
+mongoose.set('useFindAndModify', false)
+
+const MONGODB_URI = 'mongodb+srv://fullstack:fullstack@cluster0-ostce.mongodb.net/graphql?retryWrites=true'
+
+console.log('connecting to', MONGODB_URI)
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connection to MongoDB:', error.message)
+  })
 
 let authors = [
   {
