04164476cbc33ff2e03962b2c0156e6c60a8072f
Antti Mattila <72422+pihvi@users.noreply.github.com>
2019-12-28 15:04:11 +0200

    use async await in all

--- osa4/blogs.js
+++ osa4/blogs.js
@@ -32,22 +32,16 @@ module.exports.app = app
 app.use(cors())
 app.use(bodyParser.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
+app.get('/api/blogs', async (request, response) => {
+  const blogs = await Blog.find({})
+  response.json(blogs)
 })
 
-app.post('/api/blogs', (request, response) => {
+app.post('/api/blogs', async (request, response) => {
   const blog = new Blog(request.body)
   if (blog.title && blog.url) {
-    blog
-      .save()
-      .then(result => {
-        response.status(201).json(result)
-      })
+    const result = await blog.save()
+    response.status(201).json(result)
   } else {
     response.status(400).end()
   }
